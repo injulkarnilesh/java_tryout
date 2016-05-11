1871a5388e9b709bd6d63fdf506b3bdb2d4438fb
injulkarnilesh <injulkarnilesh@gmail.com>
2016-05-11 14:00:13 +0530

    Removing Algorithm from java_try as it is moved to /Algorithm

--- Algorithm/src/main/java/in/algorithm/course/part/one/week/four/priorityqueue/OrderDecider.java
+++
@@ -1,7 +0,0 @@
-package in.algorithm.course.part.one.week.four.priorityqueue;
-
-public interface OrderDecider<T extends Comparable> {
-
-    boolean isValid(Node<T> first, Node<T> second);
-
-}
--- Algorithm/src/test/java/in/algorithm/course/util/RandomBoolean.java
+++
@@ -1,9 +0,0 @@
-package in.algorithm.course.util;
-
-public class RandomBoolean {
-
-    public static boolean next() {
-        return Math.random() >= 0.5 ? true : false;
-    }
-
-}
