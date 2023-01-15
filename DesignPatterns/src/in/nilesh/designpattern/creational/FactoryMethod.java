1aeec4f331f8e524ff465f7b4eb1b36bbee5d144
Nilesh Injulkar <injulkarnilesh@gmail.com>
2023-01-15 08:56:44 +0530

    One liner description

--- DesignPatterns/src/in/nilesh/designpattern/creational/FactoryMethod.java
+++ DesignPatterns/src/in/nilesh/designpattern/creational/FactoryMethod.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/*
+Factory Method is a creational design pattern that provides an interface for creating objects in a superclass, 
+but allows subclasses to alter the type of objects that will be created.
+*/
 
 abstract class Document{
 	protected String name = "defaultname";
