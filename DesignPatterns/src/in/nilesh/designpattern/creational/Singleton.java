ff0e993c7a930ed447d9aac536405e303668e1a9
Nilesh Injulkar <injulkarnilesh@gmail.com>
2023-01-15 09:03:30 +0530

    One liner description

--- DesignPatterns/src/in/nilesh/designpattern/creational/Singleton.java
+++ DesignPatterns/src/in/nilesh/designpattern/creational/Singleton.java
@@ -1,5 +1,9 @@
 package in.nilesh.designpattern.creational;
 
+/*
+Singleton is a creational design pattern that lets you ensure 
+that a class has only one instance, while providing a global access point to this instance.
+*/
 class SingletonClassClassic {
 	private static SingletonClassClassic instance;
 	
