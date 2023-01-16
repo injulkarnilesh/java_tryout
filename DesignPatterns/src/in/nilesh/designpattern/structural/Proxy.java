a51b205c45b5c4ce954379961325853ec287daeb
Nilesh Injulkar <injulkarnilesh@gmail.com>
2023-01-16 10:34:03 +0530

    comment added

--- DesignPatterns/src/in/nilesh/designpattern/structural/Proxy.java
+++ DesignPatterns/src/in/nilesh/designpattern/structural/Proxy.java
@@ -1,5 +1,11 @@
 package in.nilesh.designpattern.structural;
 
+/*
+Proxy is a structural design pattern that lets you provide a substitute or placeholder for another object. 
+A proxy controls access to the original object, 
+allowing you to perform something either before or after the request gets through to the original object.
+*/
+
 interface InternetAccess {
 	public void grantInternetAddress();
 }
