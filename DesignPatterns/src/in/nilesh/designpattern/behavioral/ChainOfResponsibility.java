5673526a5402b0ea77a6f23e48d5aa42af1af0c3
Nilesh Injulkar <injulkarnilesh@gmail.com>
2023-01-16 15:14:26 +0530

    comment added

--- DesignPatterns/src/in/nilesh/designpattern/behavioral/ChainOfResponsibility.java
+++ DesignPatterns/src/in/nilesh/designpattern/behavioral/ChainOfResponsibility.java
@@ -1,5 +1,10 @@
 package in.nilesh.designpattern.behavioral;
 
+/*
+Chain of Responsibility is a behavioral design pattern that lets you pass requests along a chain of handlers. 
+Upon receiving a request, each handler decides either to process the request or to pass it to the next handler in the chain.
+*/
+
 class PurchaseRequest {
 	private double cost;
 	private String productName;
