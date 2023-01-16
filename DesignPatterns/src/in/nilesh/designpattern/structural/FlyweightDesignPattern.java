e14aae6c175aff494fffac1bfb029bfcea1c0d32
Nilesh Injulkar <injulkarnilesh@gmail.com>
2023-01-16 10:18:21 +0530

    comment

--- DesignPatterns/src/in/nilesh/designpattern/structural/FlyweightDesignPattern.java
+++ DesignPatterns/src/in/nilesh/designpattern/structural/FlyweightDesignPattern.java
@@ -2,7 +2,11 @@ package in.nilesh.designpattern.structural;
 
 import java.util.HashMap;
 import java.util.Map;
-
+/*
+Flyweight is a structural design pattern that 
+lets you fit more objects into the available amount of RAM by 
+sharing common parts of state between multiple objects instead of keeping all of the data in each object
+*/
 enum FONT {
 	Times, Serif, Georgia, Arial, Helvetica
 };
