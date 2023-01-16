45f9d27a5ac39f9d91b08d138323041c8bbcbcc6
Nilesh Injulkar <injulkarnilesh@gmail.com>
2023-01-16 17:05:18 +0530

    Comment description

--- DesignPatterns/src/in/nilesh/designpattern/behavioral/ObserverDesignPattern.java
+++ DesignPatterns/src/in/nilesh/designpattern/behavioral/ObserverDesignPattern.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
+Observer is a behavioral design pattern that lets you define
+a subscription mechanism to notify multiple objects about any events that happen to the object they’re observing
+*/
+
+/*
  * Abstract Observable
  */
 interface NewsPublisher {
